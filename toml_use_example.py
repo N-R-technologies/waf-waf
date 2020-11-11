abc910a77a3a20128ddd8daaa4cc63e06218d29b
noam2003 <noammiz918@gmail.com>
2020-11-11 14:36:20 +0200

    add the toml example file

---
+++ toml_use_example.py
@@ -0,0 +1,21 @@
+import toml
+def main():
+    file_content = """
+    [emails]
+    noam = "noammizrahi@gmail.com"
+    ron = "ronkonis@gmail.com"     
+    """
+    parsed_content = toml.loads(file_content)  # parsing our data into toml format
+    with open("example_file.toml", 'w') as write_file:  # open the file for writing, if doesn't exist create it
+        print("writing content to file")
+        toml.dump(parsed_content, write_file)  # write content to file
+    write_file.close()
+    with open("example_file.toml", 'r') as read_file:  # open the file for reading
+        print("content of the file:")
+        toml_content = toml.loads(read_file.read())
+        print(toml_content)
+    read_file.close()
+
+
+if __name__ == "__main__":
+    main()
