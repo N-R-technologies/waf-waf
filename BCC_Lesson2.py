beed20e10bf4036962a59f0d1911be878a3a59fa
Ron Konis <ronkonis1@gmail.com>
2020-11-13 23:39:09 +0200

    added solution for the 5th lesson in the bcc practice

--- BCC_Lesson2.py
+++ BCC_Lesson2.py
@@ -2,11 +2,13 @@ import sys
 # importing the bcc library from another folder
 sys.path.insert(1, '/usr/lib/python3/dist-packages')
 
-from bcc import BPF  # ignore this error, BPF is imported from the line above
+from bcc import BPF  # ignore this error, the line above imports BPF
 
 
 def main():
-    bpf_script = """
+    # This BPF program will print "sys_sync() called"
+    # everytime the system function "sync" is being called
+    prog = """
     int kprobe__sys_sync(void *ctx)
     {
         bpf_trace_printk("sys_sync() called\\n");
@@ -14,12 +16,14 @@ def main():
     }
     """
 
-    bpf_object = BPF(text=bpf_script)
+    # load BPF program
+    bpf = BPF(text=prog)
     try:
+        # header
         print("Tracing sys_sync()...")
         print("Press Ctrl+C to end")
-        bpf_object.trace_print()
-
+        # output
+        bpf.trace_print()
     except KeyboardInterrupt:
         print("\nTracing stopped")
 
---
+++ BCC_Lesson5.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+import sys
+# importing the bcc library from another folder
+sys.path.insert(1, '/usr/lib/python3/dist-packages')
+
+from bcc import BPF  # ignore this error, the line above imports BPF
+
+
+def main():
+    # This BPF program will print how many times the system function
+    # "sync" has been called everytime time it is being called
+    prog = """
+    #include <uapi/linux/ptrace.h>
+
+    BPF_HASH(last);
+
+    int trace_sync_count(struct pt_regs *ctx)
+    {
+        u64 *storedCounter;
+        u64 counter = 1;
+        u64 key = 0;
+
+        // attempt to read stored counter
+        storedCounter = last.lookup(&key);
+        if (storedCounter != NULL)
+        {
+            counter = *storedCounter + 1;
+            last.delete(&key);
+        }
+        bpf_trace_printk("%d\\n", counter);
+    
+        // update stored counter
+        last.update(&key, &counter);
+        return 0;
+    }
+    """
+
+    # load BPF program
+    bpf = BPF(text=prog)
+    bpf.attach_kprobe(event=bpf.get_syscall_fnname("sync"), fn_name="trace_sync_count")
+
+    # header
+    print("Counting sync calls...")
+    print("Press Ctrl+C to end")
+
+    # format output
+    while True:
+        try:
+            (task, pid, cpu, flags, ts, ms) = bpf.trace_fields()
+            print("Sync count:", ms)
+        except KeyboardInterrupt:
+            print("\nTracing stopped")
+            break
+
+
+if __name__ == "__main__":
+    main()
