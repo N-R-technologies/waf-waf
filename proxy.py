1d7c4d6fa259a58547a7c582cb9e4845bd982c4d
Ron Konis <ronkonis1@gmail.com>
2020-12-17 00:17:06 +0200

    figured out how to recieve the client's address from the flow

--- proxy.py
+++ proxy.py
@@ -1,6 +1,7 @@
 from mitmproxy import proxy, options, http
 from mitmproxy.tools.dump import DumpMaster
 import os
+# import toml # TODO: try use toml
 
 HOST = "127.0.0.1"
 PORT = 8080
@@ -12,24 +13,27 @@ class WAF:
         if os.path.exists("blacklist.txt"):
             if os.stat("blacklist.txt").st_size != 0:  # there is no need to read anything if the file is empty
                 with open("blacklist.txt", 'r') as blacklist_file:  # load the saved blacklist to the program
+                    # toml_content = toml.loads(blacklist_file.read())
                     self.blacklist = blacklist_file.read().split('\n')[:-1]
                     blacklist_file.close()
         else:
             with open("blacklist.txt", 'w') as blacklist_file:  # create the file if not exists
                 blacklist_file.close()
 
-    def http_connect(self, flow: http.HTTPFlow):
-#        if flow. in self.blacklist and flow.killable:  # check if the latest connected client is in the blacklist
-#            flow.kill()
-        pass
+    def http_connect(self, flow: http.HTTPFlow) -> None:
+        # here we check if the latest connected client is in the blacklist
+        # flow.server_conn object is used because the proxy is not reversed,
+        # so it acts as if the client is the server and the server is the client
+        if flow.server_conn.ip_address[0] in self.blacklist and flow.killable:
+            flow.kill()
 
     def response(self, flow: http.HTTPFlow) -> None:
         enter_if_statement = False
         if enter_if_statement:  # We need to replace this with "is the flow malicious?"
-            # TODO: kill the flow before appending the address to the blacklist
-            client_address = ''  # flow.address...
+            # TODO: kill the flow before appending the address to the blacklist (after merging)
+            client_address = flow.server_conn.ip_address[0]
             self.blacklist.append(client_address)
-            with open("blacklist.txt", 'a') as blacklist_file:  # save the blacklist to the program
+            with open("blacklist.txt", 'a') as blacklist_file:  # saving the blacklist outside of the program
                 blacklist_file.write(client_address + '\n')
                 blacklist_file.close()
 
