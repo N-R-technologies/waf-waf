d6a1d0009f97e6254d6c81710ba7b42ecb5248cf
noam2003 <noammiz918@gmail.com>
2020-12-18 20:09:31 +0200

    edit the proxy_info file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker run --rm -d -p 7777:80 vulnerables/web-dvwa
-firefox http://localhost:7891/login.php &
-mitmproxy -p 7891 -m reverse:http://localhost:7777 -s proxy.py
+docker run --rm -d -p 7777:80 vulnerables/web-dvwa #run the docker contain for the website
+firefox http://localhost:7891/login.php & #run the firefox browser in the address of the docker container
+mitmproxy -p 7891 -m reverse:http://localhost:7777 -s proxy.py # run the mimtproxy in reverse mode on localhost port 7777
