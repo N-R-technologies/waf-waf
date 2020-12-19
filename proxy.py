e33650d4eddf8a3fc7a93bf8fccbd6c74283a976
Ron Konis <ronkonis1@gmail.com>
2020-12-19 15:22:30 +0200

    changed code according to CR

--- proxy.py
+++ proxy.py
@@ -1,37 +1,48 @@
+import toml
+import os
 from mitmproxy import proxy, options, http
 from mitmproxy.tools.dump import DumpMaster
-import os
-import toml
 
-HOST = "127.0.0.1"
-PORT = 8080
+BLACKLIST_FILE_PATH = "blacklist.toml"
+TOML_BLACKLIST = "blacklist"  # the blacklist array's name in the toml file
+PROXY_LISTEN_HOST = "127.0.0.1"
+PROXY_LISTEN_PORT = 8080
 
 
 class WAF:
-    def __init__(self):
-        self.blacklist = []
-        if os.path.exists("blacklist.toml"):
-            if os.stat("blacklist.toml").st_size != 0:  # there is no need to read anything if the file is empty
-                self.blacklist = toml.load("blacklist.toml")['blacklist']  # load the saved blacklist to the program
-        else:
-            with open("blacklist.toml", 'w') as blacklist_file:  # create an empty blacklist toml file if not exists
-                blacklist_file.close()
+    _blacklist = set()
 
-    def response(self, flow: http.HTTPFlow) -> None:
+    def __init__(self):
+        self._load_blacklist_configuration(BLACKLIST_FILE_PATH, TOML_BLACKLIST)
+
+    def _load_blacklist_configuration(self, blacklist_file_path, toml_blacklist):
+        if os.path.exists(blacklist_file_path):
+            try:  # load the saved blacklist to the program
+                self._blacklist = set(toml.load(blacklist_file_path)[toml_blacklist])
+            except KeyError:  # toml file is empty and cannot find the "blacklist" array
+                pass
+        else:
+            open(blacklist_file_path, 'w').close()  # create an empty blacklist file if not exists
+
+    def response(self, flow: http.HTTPFlow) -> None:  # TODO: change the function to "request" after CR
+        """
+        This function will check and handle received malicious requests
+        :param flow: The user's request
+        :type flow: http.HTTPFlow
+        :return: None
+        """
         # check if the latest connected client is in the blacklist
-        # flow.server_conn object is used because the proxy is not reversed,
-        # so it acts as if the client is the server and the server is the client
-        if flow.server_conn.ip_address[0] in self.blacklist and flow.killable:
+        if flow.server_conn.ip_address[0] in self._blacklist and flow.killable:
             flow.kill()
         else:
             enter_if_statement = False
-            if enter_if_statement:  # we need to replace this with "is the flow malicious?"
+            if enter_if_statement:  # TODO: replace this with "is the flow malicious?"
                 if flow.killable:
                     flow.kill()
                 attacker_ip_address = flow.server_conn.ip_address[0]
-                self.blacklist.append(attacker_ip_address)
-                with open("blacklist.toml", 'w') as blacklist_file:  # saving the blacklist outside of the program
-                    toml.dump({'blacklist': self.blacklist}, blacklist_file)
+                self._blacklist.add(attacker_ip_address)
+                with open(BLACKLIST_FILE_PATH, 'w') as blacklist_file:  # saving the blacklist outside of the program
+                    toml.dump({TOML_BLACKLIST: self._blacklist}, blacklist_file)
                     blacklist_file.close()
 
 
@@ -39,7 +50,7 @@ addons = [
     WAF()
 ]
 
-options = options.Options(listen_host=HOST, listen_port=PORT)
+options = options.Options(listen_host=PROXY_LISTEN_HOST, listen_port=PROXY_LISTEN_PORT)
 options.add_option("body_size_limit", int, 0, "")
 options.add_option("intercept_active", bool, False, "")
 options.add_option("keep_host_header", bool, True, "")
