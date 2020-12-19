fe850d13165f11adc2137ce6b01a4faebf0e8524
Ron Konis <ronkonis1@gmail.com>
2020-12-19 22:50:51 +0200

    fixed code according to CR

--- proxy.py
+++ proxy.py
@@ -4,25 +4,28 @@ from mitmproxy import proxy, options, http
 from mitmproxy.tools.dump import DumpMaster
 
 BLACKLIST_FILE_PATH = "blacklist.toml"
-TOML_BLACKLIST = "blacklist"  # the blacklist array's name in the toml file
 PROXY_LISTEN_HOST = "127.0.0.1"
-PROXY_LISTEN_PORT = 8080
+PROXY_LISTEN_PORT = 80800
 
 
 class WAF:
     _blacklist = set()
 
     def __init__(self):
-        self._load_blacklist_configuration(BLACKLIST_FILE_PATH, TOML_BLACKLIST)
+        self._load_blacklist_configuration(BLACKLIST_FILE_PATH)
 
-    def _load_blacklist_configuration(self, blacklist_file_path, toml_blacklist):
+    def _load_blacklist_configuration(self, blacklist_file_path):
         if os.path.exists(blacklist_file_path):
-            try:  # load the saved blacklist to the program
-                self._blacklist = set(toml.load(blacklist_file_path)[toml_blacklist])
-            except KeyError:  # toml file is empty and cannot find the "blacklist" array
-                pass
+            # "None" can't be loaded into a set, so an empty string is being used instead
+            self._blacklist = set(toml.load(blacklist_file_path).get("blacklist", ""))
         else:
-            open(blacklist_file_path, 'w').close()  # create an empty blacklist file if not exists
+            open(blacklist_file_path, 'w').close()
+
+    def _add_client_to_blacklist(self, attacker_ip_address):
+        self._blacklist.add(attacker_ip_address)
+        with open(BLACKLIST_FILE_PATH, 'w') as blacklist_file:
+            toml.dump({"blacklist": self._blacklist}, blacklist_file)
+            blacklist_file.close()
 
     def response(self, flow: http.HTTPFlow) -> None:  # TODO: change the function to "request" after CR
         """
@@ -31,19 +34,16 @@ class WAF:
         :type flow: http.HTTPFlow
         :return: None
         """
-        # check if the latest connected client is in the blacklist
-        if flow.server_conn.ip_address[0] in self._blacklist and flow.killable:
+        client_ip_address = flow.server_conn.ip_address[0]  # TODO: probably change "server_conn" to "client_conn" after changing to "request"
+        is_client_blocked = client_ip_address in self._blacklist
+        if is_client_blocked and flow.killable:
             flow.kill()
         else:
             enter_if_statement = False
             if enter_if_statement:  # TODO: replace this with "is the flow malicious?"
                 if flow.killable:
                     flow.kill()
-                attacker_ip_address = flow.server_conn.ip_address[0]
-                self._blacklist.add(attacker_ip_address)
-                with open(BLACKLIST_FILE_PATH, 'w') as blacklist_file:  # saving the blacklist outside of the program
-                    toml.dump({TOML_BLACKLIST: self._blacklist}, blacklist_file)
-                    blacklist_file.close()
+                self._add_client_to_blacklist(client_ip_address)
 
 
 addons = [
