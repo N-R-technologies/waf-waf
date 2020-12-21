cc36d0fdeefa28dbbb4457d38232ca6c198ecad5
noam2003 <noammiz918@gmail.com>
2020-12-21 14:45:54 +0200

    remove the city_search project file

--- citysearch_project/cities/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import HomePageView, SearchResultsView
-
-urlpatterns = [
-    path('search/', SearchResultsView.as_view(), name='search_results'),
-    path('', HomePageView.as_view(), name='home'),
-]
