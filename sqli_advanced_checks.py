ba2b81eb345823cea1a6114144c3d72120307342
noam2003 <noammiz918@gmail.com>
2020-12-22 11:48:48 +0200

    make for loops in detector and change return type of detective functions

--- sqli_advanced_checks.py
+++ sqli_advanced_checks.py
@@ -2,175 +2,19 @@ import re
 from risk_level import RiskLevel
 
 
-class SqlIAdvancedCheck:
-    @staticmethod
-    def check_delete(sub_statement):
-        """
-        function check if this is a delete statement, and return the dangerous level
-        :param sub_statement: the statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.MEDIUM_RISK) if re.search(r"""delete\s+.+?\s+from\s+.+""", sub_statement)\
-            else (False, RiskLevel.NO_RISK)
+class SqlIAdvancedChecks:
 
     @staticmethod
-    def check_create(sub_statement):
-        """
-        function check if the query is a create sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.MEDIUM_RISK) if re.search(r"""create\s+(?P<createinfo>database|table|index|
-        (?:or\s+replace\s+)?view)\s+.+""", sub_statement) else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_alter(sub_statement):
-        """
-        function check if the query is a alter table sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.MEDIUM_RISK) if re.search(r"""alter\s+table\s+.+?\s+(?:add|drop\s+column)\s+.+""",
-                                                        sub_statement) else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_drop(sub_statement):
-        """
-        function check if the query is a drop sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-            """
-        return (True, RiskLevel.LARGE_RISK) if re.search(r"""drop\s+(?P<deleteinfo>database|index|table|view)\s+.+""",
-                                                       sub_statement) else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_exist(sub_statement):
-        """
-        function check if the query is a where sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.VERY_LITTLE_RISK) if re.search(r"""where\s+exists""", sub_statement) \
-            else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_update(sub_statement):
-        """
-        function check if the query is a update sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.MEDIUM_RISK) if re.search(r"""update\s+.+?\s+set\s+.+""", sub_statement) \
-            else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_truncate(sub_statement):
-        """
-        function check if the query is a truncate table sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.MEDIUM_RISK) if re.search(r"""truncate\s+table\s+.+""", sub_statement) \
-            else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_insert(sub_statement):
-        """
-        function check if the query is a insert into sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.MEDIUM_RISK) if re.search(
-            r"""insert\s+into\s+(?:'[^']+?'|\"[^\"]+?\"|\[[^\]]+?\]|\w+)(?:\s*\(.+?\)\s*|\s+)values\s*\(.+\)""",
-            sub_statement) else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_select_union(sub_statement):
-        """function check if the query is a select union sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.MEDIUM_RISK) if \
-            re.search(r"""select\s+.+?\s+from\s+.+?\s+union(?:\s+all)?\s+select\s+.+?\s+from\s+.+""",
-                      sub_statement) else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_select_into(sub_statement):
-        """
-        function check if the query is a select into sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.MEDIUM_RISK) if re.search(r"""select\s+.+?\s+into\s+.+?\s+from\s+.+""", sub_statement) \
-            else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_select_from(sub_statement):
-        """
-        function check if the query is a select from sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        return (True, RiskLevel.MEDIUM_RISK) if re.search(r"""select.+?from\s+.+""", sub_statement) \
-            else (False, RiskLevel.NO_RISK)
-
-    @staticmethod
-    def check_grant_revoke(sub_statement):
+    def grant_revoke(request):
         """
         function check if the query is a grant or revoke sql statement
-        :param sub_statement: the sub statement
-        :type sub_statement: string
-        :return: if its find
+        :param request: the sub statement
+        :type request: string
         :return: the risk level
-        :rtype: boolean
         :rtype: integer
         """
         grant_revoke_statement = re.search(r"""(?:grant|revoke)(?P<permissions>.+?)on\s+.+?\s+(?:to|from)\s+.+?""",
-                                           sub_statement)
+                                           request)
         risk_level = 0
         if grant_revoke_statement:
 
@@ -194,54 +38,8 @@ class SqlIAdvancedCheck:
                     elif "select" in permission_lst:
                         risk_level = RiskLevel.LOW_RISK
             if not risk_level:  # if its still zero make it LOW_RISK
-                return (True, RiskLevel.VERY_LITTLE_RISK)
-            return (True, risk_level)
-        return (False, RiskLevel.NO_RISK)
+                return RiskLevel.VERY_LITTLE_RISK
+            return risk_level
+        return RiskLevel.NO_RISK
 
-    @staticmethod
-    def check_or(or_statement):
-        """
-        function check the or custom, and if its true, and return the risk level of the query
-        :param logic_statement: the logical statement in the or
-        :type logic_statement: string
-        :return: if its find
-        :return: the risk level
-        :rtype: boolean
-        :rtype: integer
-        """
-        risk_level = 0
-        logic_statement = re.search(r"""(?P<statement>(?:not\s+)*\s*(?P<operators>.+?<[^=>]+|[^=!<>]+=[^=]+|[^<]
-                    +?>[^=]+|.+?(?:==|<=|>=|!=|<>).+?)\s*|(?:not\s+)*.+?\s+(?:(?P<like>like\s+.+)|
-                    (?P<betweenand>between\s+.+?and\s+.+)|(?P<in>in\s*\(.+\))))""", or_statement)
-        if logic_statement:
-            statement = logic_statement.group("statement")
-            not_count = statement.count("not")
-            statement = statement.replace("not", "")
-            is_positive = False
-            if not_count % 2 == 0:
-                is_positive = True
-            if logic_statement.group("operators"):
-                if re.search(r"""\b=\b""", statement):
-                    statement = statement.replace('=', "==")
-                elif "<>" in statement:
-                    statement = statement.replace("<>", "!=")
-            elif logic_statement.group("like"):
-                statement = statement.replace("like", "==")
-            elif logic_statement.group("betweenand"):
-                middle_value = statement[:statement.find("between")]
-                lower_value = statement[statement.find("between") + len("between"): statement.find("and")]
-                higher_value = statement[statement.find("and") + len("and"):]
-                statement = lower_value + " <= " + middle_value + " <= " + higher_value
-            try:
-                result = eval(statement)
-                print(statement)
-                if not is_positive:  # eval's result should be the opposite (True -> False | False -> True)
-                    result = not result
-                if result:  # checks if the or statement returns true
-                    risk_level = RiskLevel.LARGE_RISK
-                else:
-                    risk_level = RiskLevel.MEDIUM_RISK
-            except:  # means that the or statement is incorrect
-                risk_level = RiskLevel.VERY_LITTLE_RISK
-            return (True, risk_level)
-        return (False, RiskLevel.NO_RISK)
+
--- sqlinjection_detector.py
+++ sqlinjection_detector.py
@@ -1,70 +1,49 @@
 import inspect
 from risk_level import RiskLevel
 import sqlinjection_info
-from sqli_basic_checks import SqlIBasicCheck
-from sqli_advanced_checks import SqlIAdvancedCheck
+from sqli_basic_checks import SqlIBasicChecks
+from sqli_advanced_checks import SqlIAdvancedChecks
+
+
+NUM_OF_RISKS = 9
 
 
 def detector(request):
     """
     this is the main function of the library, in the proxy server you just need to run it over every request
-    packet that reach the server
+    packet that reach the server, return the attack detected and the info about it
     :param request: the request that goes into the server
     :type request: string
-    :return: if the detector detect sql injection in thr the packet
-    :return: the estimated risk level of the packer
-    :return: the information about the attack that occurred
-    :rtype: boolean
-    :rtype: integer
+    :return: tuple of list of risk level and the info about the attack
+    :rtype: tuple
+    """
+    risk_conclusion = [0] * NUM_OF_RISKS
+    all_risks_info = ""
+
+    basic_checks = inspect.getmembers(SqlIBasicChecks, predicate=inspect.isfunction)
+    for basic_check_name, basic_check in basic_checks:
+        est_risk_from_basic = basic_check(request)
+        if est_risk_from_basic != RiskLevel.NO_RISK:
+            print(basic_check_name)
+            risk_conclusion[int(est_risk_from_basic)-1] += 1
+            all_risks_info += sqlinjection_info.deep_info[basic_check_name]
+
+    #  finish all the basic checks, now the advanced check take part
+    advanced_checks = inspect.getmembers(SqlIAdvancedChecks, predicate=inspect.isfunction)
+    for advanced_check_name, advanced_check in advanced_checks:
+        est_risk_from_advanced = advanced_check(request)
+        if est_risk_from_advanced != RiskLevel.NO_RISK:
+            risk_conclusion[int(est_risk_from_advanced) - 1] += 1
+            all_risks_info += sqlinjection_info.deep_info[advanced_check_name]
+    return concat_info(all_risks_info), risk_conclusion
+
+
+def concat_info(risks_info):
+    """
+    function concat all the info about the attack
+    :param risks_info: all the info about the detected risks
+    :type risks_info: string
+    :return: the concat information about the attack
     :rtype: string
     """
-    counter_finds_basic_checks = 0
-    counter_finds_advanced_checks = 0
-    est_basic_risk_level = 0
-    est_advanced_risk_level = 0
-    risk_to_add = 0
-    statements_list = []
-    attack_info = ""
-    for function in inspect.getmembers(SqlIBasicCheck, predicate=inspect.isfunction):
-        if function[1](request):
-            counter_finds_basic_checks += 1
-            attack_info += sqlinjection_info.deep_info[function[0]]
-    if counter_finds_basic_checks == 1:
-        est_basic_risk_level = RiskLevel.VERY_LITTLE_RISK
-    elif (counter_finds_basic_checks > 1) and (counter_finds_basic_checks < 3):
-        est_basic_risk_level = RiskLevel.VERY_LOW_RISK
-    elif counter_finds_basic_checks >= 3:
-        est_basic_risk_level = RiskLevel.LOW_RISK
-    #  finish all the basic checks, now the advanced check take part
-    if ';' in request:
-        statements_list = request.split(';')
-    else:
-        statements_list.append(request)
-    if statements_list[-1] == '':
-        statements_list = statements_list[:-1]
-    for sub_statement in statements_list:  # for example if the request contains couple of queries like:
-        #  or 1 = 1; drop table table_name
-        sub_statement = sub_statement.strip()  # remove spaces from begin and end of the sub_statement
-        # for every sub statement check the advanced detection functions
-        for or_statement in sub_statement.split("or")[1:]:  # checks for every statement if its an 'or' statement
-            find, risk_to_add = SqlIAdvancedCheck.check_or(or_statement)
-            if find:
-                est_advanced_risk_level += risk_to_add
-                counter_finds_advanced_checks += 1
-                attack_info += sqlinjection_info.deep_info["risk_to_add"]
-        for function in inspect.getmembers(SqlIAdvancedCheck, predicate=inspect.isfunction):
-            if function[0] != "check_or":
-                find, risk_to_add = function[1](sub_statement)
-                if find:
-                    est_advanced_risk_level += risk_to_add
-                    counter_finds_advanced_checks += 1
-                    attack_info += sqlinjection_info.deep_info[function[0]]
-    try:
-        est_advanced_risk_level /= counter_finds_advanced_checks
-    except ZeroDivisionError:
-        return (False, None, None)
-    if est_advanced_risk_level >= RiskLevel.MEDIUM_RISK or est_basic_risk_level >= RiskLevel.VERY_LOW_RISK:
-        est_risk_level = (est_basic_risk_level + est_advanced_risk_level) / 2
-        info = sqlinjection_info.general_info + attack_info + sqlinjection_info.links_for_info
-        return (True, est_risk_level, info)
-    return (False, None, None)
+    return sqlinjection_info.general_info + '\n' + risks_info + '\n' + sqlinjection_info.links_for_info
