5f0db8b5b2c91ecba773a3862d91854a52212e39
noam2003 <noammiz918@gmail.com>
2020-12-26 11:24:10 +0200

    finish building the send email + log handler classes

--- email_sender.py
+++ email_sender.py
@@ -1,41 +1,56 @@
-import yagmail
 import smtplib
+import toml
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
 
 
 class EmailSender:
     @staticmethod
     def get_email_addresses():
         """ This function will read the users email addresses from the toml configuration file """
-        email_addresses = dict()
-        # reading data from emails.toml here
-        # see "toml_use_example" to check how to read data from toml files
-        return email_addresses
+        with open("email_addresses.toml", 'r') as email_file:  # open the file for reading
+            print("Reading content from file...")
+            email_addresses = toml.loads(email_file.read())
+            email_file.close()
+            emails_lst = []
+            for name in email_addresses["emails"]:
+                emails_lst.append(email_addresses["emails"][name])
+        return emails_lst
 
     @staticmethod
     def send_emails(daily_log):
-        """ This function will send the daily log to the users email addresses
-        :param email_addresses: the email addresses of the users
+        """
+        This function will send the daily log to the users email addresses
         :param daily_log: the daily log file to send
-        :type email_addresses: list
         :type daily_log: pdf file
         :return: if it succeed to send the emails
-        :rtype: boolean"""
-        email_addresses = EmailSender.get_email_addresses()
-        email_addresses.append("bot email address")  # remove this line later. it is used to send an email to ourselves
-        email_addresses['bot name'] = "bot email address"
-        FROM = "bot email address"
-        TO = email_addresses
-        SUBJECT = "Daily log"
-        TEXT = daily_log
-        # Prepare actual message
-        message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-        
-        """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+        :rtype: boolean
+        """
+        daily_log_mail = MIMEMultipart()
+        daily_log_mail['From'] = "WAFDetectiveBot@gmail.com"
+        daily_log_mail['To'] = ", ".join(EmailSender.get_email_addresses())
+        daily_log_mail['Date'] = formatdate(localtime=True)
+        daily_log_mail['Subject'] = "Daily log"
+        daily_log_description = "Hello there, this is your daily log from the WAF\n" \
+               "If you have any problems dont answer this email. send your question to: noammiz918@gmail.com\n" \
+               "have a good one!"
+
+        daily_log_mail.attach(MIMEText(daily_log_description))
+        with open(daily_log, "rb") as log_to_send:
+            part = MIMEApplication(
+                log_to_send.read(),
+                Name=basename(daily_log)
+            )
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(daily_log)
+        daily_log_mail.attach(part)
         try:
             server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
             server_ssl.ehlo()
-            server_ssl.login("bot email address", "bot email password")
-            server_ssl.sendmail(FROM, TO, message)
+            server_ssl.login("wafdetectivebot@gmail.com", "wafbot2003")
+            server_ssl.sendmail(daily_log_mail['From'], daily_log_mail['To'], daily_log_mail.as_string())
             server_ssl.close()
             return True
         except Exception:
--- log_handler.py
+++ log_handler.py
@@ -1,4 +1,3 @@
-
 from fpdf import FPDF
 from email_sender import EmailSender
 
@@ -7,6 +6,7 @@ class LogHandler:
     _daily_log = ""
 
     def __init__(self):
+        """function initialize the pdf log document and set the title"""
         self._daily_log = FPDF()
         self._daily_log.add_page()
         self._daily_log.set_font("Times", 'BU', size=30)
@@ -15,12 +15,22 @@ class LogHandler:
         self._daily_log.set_font("Arial", size=10)
 
     def write_pdf(self, attack_info):
-        attack_lst = info.split('\n')
+        """
+        function write the attack info into the log file
+        :param attack_info: the attack info
+        :return: None
+        """
+        attack_lst = attack_info.split('\n')
         for row in attack_lst:
             self._daily_log.cell(200, 10, txt=row,
                             ln=2, align='L')
         self._daily_log.image("icon.jpeg", x=80, w=70, h=80)
 
     def save_send(self):
+        """
+        function save the log document and sent it to the users
+        :return: if the email send action succeeded
+        :rtype: boolean
+        """
         self._daily_log.output("daily_log.pdf")
-        #EmailSender.send_emails("daily_log.pdf")
+        return EmailSender.send_emails("daily_log.pdf")
