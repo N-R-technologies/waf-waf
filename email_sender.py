f97c217de954f7f84704ee31498ee710953975c1
Ron Konis <ronkonis1@gmail.com>
2020-12-27 19:50:26 +0200

    added my email and generally made the code cleaner

--- email_sender.py
+++ email_sender.py
@@ -1,57 +1,67 @@
-import smtplib
 import toml
-from os.path import basename
+import os
+import smtplib
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formatdate
 
+EMAILS_FILE_PATH = "email_addresses.toml"
+WAF_BOT_ADDRESS = "WAFDetectiveBot@gmail.com"
+WAF_BOT_PASS = "wafbot2003"
+LOG_SUBJECT = "Daily log"
+LOG_DESCRIPTION = "Hello there, this is your daily log from the WAF.\nIf you have any problems you can " \
+                  "contact us anytime.\nIn addition, if you recognize that you have been attacked and we " \
+                  "were unable to identify it, we would like you to send a full report of the case.\n\n" \
+                  "Please do not reply to this address! You will not receive any response.\n" \
+                  "Instead, contact noammiz918@gmail.com or ronkonis1@gmail.com.\nHave a good one!"
+
 
 class EmailSender:
     @staticmethod
-    def get_email_addresses():
-        """ This function will read the users email addresses from the toml configuration file """
-        with open("email_addresses.toml", 'r') as email_file:  # open the file for reading
-            print("Reading content from file...")
-            email_addresses = toml.loads(email_file.read())
-            email_file.close()
-            emails_lst = []
-            for name in email_addresses["emails"]:
-                emails_lst.append(email_addresses["emails"][name])
-        return emails_lst
-
-    @staticmethod
-    def send_emails(daily_log):
+    def send_log(daily_log):
         """
         This function will send the daily log to the users email addresses
-        :param daily_log: the daily log file to send
-        :type daily_log: pdf file
-        :return: if it succeed to send the emails
+        :param daily_log: the daily log's file path
+        :type daily_log: string
+        :return: if the function succeeded sending the emails
         :rtype: boolean
         """
-        daily_log_mail = MIMEMultipart()
-        daily_log_mail['From'] = "WAFDetectiveBot@gmail.com"
-        daily_log_mail['To'] = ", ".join(EmailSender.get_email_addresses())
-        daily_log_mail['Date'] = formatdate(localtime=True)
-        daily_log_mail['Subject'] = "Daily log"
-        daily_log_description = "Hello there, this is your daily log from the WAF\n" \
-               "If you have any problems dont answer this email. send your question to: noammiz918@gmail.com\n" \
-               "have a good one!"
+        email_addresses = EmailSender.load_emails_configuration(EMAILS_FILE_PATH)
+        if len(email_addresses) > 0:
+            daily_log_mail = MIMEMultipart()
+            daily_log_mail['From'] = WAF_BOT_ADDRESS
+            daily_log_mail['To'] = ", ".join(email_addresses)
+            daily_log_mail['Date'] = formatdate(localtime=True)
+            daily_log_mail['Subject'] = LOG_SUBJECT
+            daily_log_description = LOG_DESCRIPTION
 
-        daily_log_mail.attach(MIMEText(daily_log_description))
-        with open(daily_log, "rb") as log_to_send:
-            part = MIMEApplication(
-                log_to_send.read(),
-                Name=basename(daily_log)
-            )
-        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(daily_log)
-        daily_log_mail.attach(part)
-        try:
-            server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-            server_ssl.ehlo()
-            server_ssl.login("wafdetectivebot@gmail.com", "wafbot2003")
-            server_ssl.sendmail(daily_log_mail['From'], daily_log_mail['To'], daily_log_mail.as_string())
-            server_ssl.close()
-            return True
-        except Exception:
-            return False
+            daily_log_mail.attach(MIMEText(daily_log_description))
+            with open(daily_log, "rb") as log_to_send:
+                part = MIMEApplication(log_to_send.read(), Name=os.path.basename(daily_log))
+            part['Content-Disposition'] = 'attachment; filename="%s"' % os.path.basename(daily_log)
+            daily_log_mail.attach(part)
+
+            try:
+                server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+                server_ssl.ehlo()
+                server_ssl.login(WAF_BOT_ADDRESS, WAF_BOT_PASS)
+                server_ssl.sendmail(daily_log_mail['From'], daily_log_mail['To'], daily_log_mail.as_string())
+                server_ssl.close()
+                return True
+            except Exception:
+                return False
+        return True
+
+    @staticmethod
+    def load_emails_configuration(emails_file_path):
+        emails = dict()
+        if os.path.exists(emails_file_path):
+            emails = toml.load(emails_file_path).get("emails", {})
+        else:
+            open(emails_file_path, 'w').close()
+
+        email_addresses = set()
+        for email_address in emails.values():
+            email_addresses.add(email_address)
+        return email_addresses
--- log_handler.py
+++ log_handler.py
@@ -6,31 +6,32 @@ class LogHandler:
     _daily_log = ""
 
     def __init__(self):
-        """function initialize the pdf log document and set the title"""
+        """
+        This function will initialize the pdf log document and will set the title
+        """
         self._daily_log = FPDF()
         self._daily_log.add_page()
         self._daily_log.set_font("Times", 'BU', size=30)
-        self._daily_log.cell(200, 10, txt="Daily Log WAF",
-                 ln=1, align='C')
+        self._daily_log.cell(200, 10, txt="Daily Log WAF", ln=1, align='C')
         self._daily_log.set_font("Arial", size=10)
 
-    def write_pdf(self, attack_info):
+    def write_to_log(self, attack_info):
         """
-        function write the attack info into the log file
-        :param attack_info: the attack info
+        This function will write the attack information into the daily log file
+        :param attack_info: the attack information
+        :type attack_info: string
         :return: None
         """
         attack_lst = attack_info.split('\n')
         for row in attack_lst:
-            self._daily_log.cell(200, 10, txt=row,
-                            ln=2, align='L')
+            self._daily_log.cell(200, 10, txt=row, ln=2, align='L')
         self._daily_log.image("icon.jpeg", x=80, w=70, h=80)
 
     def save_send(self):
         """
-        function save the log document and sent it to the users
+        This function will save the daily log document and will send it to the users
         :return: if the email send action succeeded
         :rtype: boolean
         """
         self._daily_log.output("daily_log.pdf")
-        return EmailSender.send_emails("daily_log.pdf")
+        return EmailSender.send_log("daily_log.pdf")
