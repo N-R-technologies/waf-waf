37672371e600239b3af8a5327e4bd9afd7ded4e3
Ron Konis <ronkonis1@gmail.com>
2020-12-30 22:05:08 +0200

    changed code due to CR

--- email_sender.py
+++ email_sender.py
@@ -1,15 +1,15 @@
 import toml
 import os
+from datetime import date
 import smtplib
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formatdate
 
-EMAILS_FILE_PATH = "email_addresses.toml"
-WAF_BOT_ADDRESS = "WAFDetectiveBot@gmail.com"
-WAF_BOT_PASS = "wafbot2003"
-LOG_SUBJECT = "Daily log"
+BOT_ADDRESS_FILE_PATH = "bot_address.toml"
+USER_ADDRESSES_FILE_PATH = "user_addresses.toml"
+LOG_SUBJECT = "Daily log - "
 LOG_DESCRIPTION = "Hello there, this is your daily log from the WAF.\nIf you have any problems you can " \
                   "contact us anytime.\nIn addition, if you recognize that you have been attacked and we " \
                   "were unable to identify it, we would like you to send a full report of the case.\n\n" \
@@ -18,8 +18,13 @@ LOG_DESCRIPTION = "Hello there, this is your daily log from the WAF.\nIf you hav
 
 
 class EmailSender:
-    @staticmethod
-    def send_log(daily_log):
+    _bot_address = ""
+    _bot_pass = ""
+
+    def __init__(self):
+        self._load_bot_address_configuration(BOT_ADDRESS_FILE_PATH)
+
+    def send_log(self, daily_log):
         """
         This function will send the daily log to the users email addresses
         :param daily_log: the daily log's file path
@@ -27,41 +32,48 @@ class EmailSender:
         :return: if the function succeeded sending the emails
         :rtype: boolean
         """
-        email_addresses = EmailSender.load_emails_configuration(EMAILS_FILE_PATH)
-        if len(email_addresses) > 0:
+        user_addresses = self._load_user_addresses_configuration(USER_ADDRESSES_FILE_PATH)
+        if len(user_addresses) > 0:
             daily_log_mail = MIMEMultipart()
-            daily_log_mail['From'] = WAF_BOT_ADDRESS
-            daily_log_mail['To'] = ", ".join(email_addresses)
-            daily_log_mail['Date'] = formatdate(localtime=True)
-            daily_log_mail['Subject'] = LOG_SUBJECT
+            daily_log_mail["From"] = self._bot_address
+            daily_log_mail["To"] = ", ".join(user_addresses)
+            daily_log_mail["Date"] = formatdate(localtime=True)
+            daily_log_mail["Subject"] = LOG_SUBJECT + date.today().strftime("%d/%m/%Y")
             daily_log_description = LOG_DESCRIPTION
 
             daily_log_mail.attach(MIMEText(daily_log_description))
             with open(daily_log, "rb") as log_to_send:
                 part = MIMEApplication(log_to_send.read(), Name=os.path.basename(daily_log))
-            part['Content-Disposition'] = 'attachment; filename="%s"' % os.path.basename(daily_log)
+            part["Content-Disposition"] = 'attachment; filename="%s"' % os.path.basename(daily_log)
             daily_log_mail.attach(part)
 
             try:
                 server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
                 server_ssl.ehlo()
-                server_ssl.login(WAF_BOT_ADDRESS, WAF_BOT_PASS)
-                server_ssl.sendmail(daily_log_mail['From'], daily_log_mail['To'], daily_log_mail.as_string())
+                server_ssl.login(self._bot_address, self._bot_pass)
+                server_ssl.sendmail(daily_log_mail["From"], daily_log_mail["To"], daily_log_mail.as_string())
                 server_ssl.close()
                 return True
             except Exception:
                 return False
         return True
 
-    @staticmethod
-    def load_emails_configuration(emails_file_path):
-        emails = dict()
-        if os.path.exists(emails_file_path):
-            emails = toml.load(emails_file_path).get("emails", {})
+    def _load_bot_address_configuration(self, bot_address_file_path):
+        if os.path.exists(bot_address_file_path):
+            bot_address = toml.load(bot_address_file_path)
+            self._bot_address = bot_address["address"]
+            self._bot_pass = bot_address["pass"]
         else:
-            open(emails_file_path, 'w').close()
+            raise FileNotFoundError
 
-        email_addresses = set()
-        for email_address in emails.values():
-            email_addresses.add(email_address)
-        return email_addresses
+    def _load_user_addresses_configuration(self, user_addresses_file_path):
+        user_addresses = dict()
+        if os.path.exists(user_addresses_file_path):
+            user_addresses = toml.load(user_addresses_file_path).get("addresses", {})
+        else:
+            open(user_addresses_file_path, 'w').close()
+
+        addresses = set()
+        for address in user_addresses.values():
+            addresses.add(address)
+        return addresses
--- log_handler.py
+++ log_handler.py
@@ -1,8 +1,9 @@
 from fpdf import FPDF
-from email_sender import EmailSender
+
+LOG_FILE_PATH = "daily_log.pdf"
 
 
-class LogHandler:
+class LogWriter:
     _daily_log = ""
 
     def __init__(self):
@@ -20,18 +21,14 @@ class LogHandler:
         This function will write the attack information into the daily log file
         :param attack_info: the attack information
         :type attack_info: string
-        :return: None
         """
         attack_list = attack_info.split('\n')
         for row in attack_list:
             self._daily_log.cell(200, 10, txt=row, ln=2, align='L')
         self._daily_log.image("icon.jpeg", x=80, w=70, h=80)
 
-    def save_send(self):
+    def save_log(self):
         """
-        This function will save the daily log document and will send it to the users
-        :return: if the email send action succeeded
-        :rtype: boolean
+        This function will save the daily log document
         """
-        self._daily_log.output("daily_log.pdf")
-        return EmailSender.send_log("daily_log.pdf")
+        self._daily_log.output(LOG_FILE_PATH)
