1fb016587997127cae7edd71c52e30d8760f40a0
Ron Konis <ronkonis1@gmail.com>
2020-12-30 20:12:17 +0200

    changed the sql injection detective due to CR. there are still more things to do

---
+++ detectors/sql_injection/detector.py
@@ -0,0 +1,56 @@
+import inspect
+import info
+from basic_checks import BasicChecks
+from advanced_checks import AdvancedChecks
+from risk_level import RiskLevel
+
+
+class Detector:
+    def detect(self, request):
+        """
+        This function will be called by the detective for every request that should reach the server
+        It will return the risk levels of SQL Injection attack and information about it
+        :param request: the request that goes into the server
+        :type request: string
+        :return: tuple of risk levels list and the information about the attack
+        :rtype: tuple
+        """
+        findings_graph = [0] * len(RiskLevel)
+        all_risks_info = ""
+
+        basic_checks = inspect.getmembers(BasicChecks, predicate=inspect.isfunction)
+        for basic_check_name, basic_check in basic_checks:
+            basic_check_result = basic_check(request)
+            if basic_check_result > RiskLevel.NO_RISK:
+                findings_graph[basic_check_result] += 1
+                all_risks_info += info.deep_info[basic_check_name]
+
+        advanced_checks = inspect.getmembers(AdvancedChecks, predicate=inspect.isfunction)
+        for advanced_check_name, advanced_check in advanced_checks:
+            advanced_check_result = advanced_check(request)
+            if advanced_check_result > RiskLevel.NO_RISK:
+                findings_graph[advanced_check_result] += 1
+                all_risks_info += info.deep_info[advanced_check_name]
+
+        return Detector.summarize_info(all_risks_info), findings_graph
+
+    @staticmethod
+    def summarize_info(risks_info):
+        """
+        This function will summarize all the information about the attack
+        :param risks_info: all the information about the detected risks
+        :type risks_info: string
+        :return: the summarized information about the attack
+        :rtype: string
+        """
+        if not risks_info:
+            risks_info = "* No risks detected\n"
+        risks_list = []
+        risks_list.append("General Information SQL Injection:\n" + info.general_info + "\n\nDetected risks:\n")
+        risks_list.append(risks_info + '\n')
+        risks_list.append(info.links_for_info)
+        return risks_list
+
+    @property
+    def category(self):
+        return "SQL Injection"
