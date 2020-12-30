35f67848a989f444a0b534522f4df50a34aafef8
Ron Konis <ronkonis1@gmail.com>
2020-12-30 18:20:53 +0000

    updated detector.py

--- detectors/sql_injection/detector.py
+++ detectors/sql_injection/detector.py
@@ -12,27 +12,27 @@ class Detector:
         It will return the risk levels of SQL Injection attack and information about it
         :param request: the request that goes into the server
         :type request: string
-        :return: tuple of risk levels list and the information about the attack
+        :return: risk levels of SQL Injection and information about the attack
         :rtype: tuple
         """
-        findings_graph = [0] * len(RiskLevel)
+        risk_findings = [0] * len(RiskLevel)
         all_risks_info = ""
 
         basic_checks = inspect.getmembers(BasicChecks, predicate=inspect.isfunction)
         for basic_check_name, basic_check in basic_checks:
             basic_check_result = basic_check(request)
             if basic_check_result > RiskLevel.NO_RISK:
-                findings_graph[basic_check_result] += 1
+                risk_findings[basic_check_result] += 1
                 all_risks_info += info.deep_info[basic_check_name]
 
         advanced_checks = inspect.getmembers(AdvancedChecks, predicate=inspect.isfunction)
         for advanced_check_name, advanced_check in advanced_checks:
             advanced_check_result = advanced_check(request)
             if advanced_check_result > RiskLevel.NO_RISK:
-                findings_graph[advanced_check_result] += 1
+                risk_findings[advanced_check_result] += 1
                 all_risks_info += info.deep_info[advanced_check_name]
 
-        return Detector.summarize_info(all_risks_info), findings_graph
+        return Detector.summarize_info(all_risks_info), risk_findings
 
     @staticmethod
     def summarize_info(risks_info):
