008c5f280c80a1ea854c03714304d7dedf33f744
noam2003 <noammiz918@gmail.com>
2021-01-09 14:24:32 +0200

    add the run.sh files with parameters

--- run_dvwa.sh
+++
@@ -1,3 +0,0 @@
-docker run --rm -d -p 7777:80 vulnerables/web-dvwa #run the docker contain for the website
-firefox http://localhost:7891/login.php & #run the firefox browser in the address of the docker container
-mitmproxy -p 7891 -m reverse:http://localhost:7777 -s proxy.py # run the mimtproxy in reverse mode on localhost port 7777
