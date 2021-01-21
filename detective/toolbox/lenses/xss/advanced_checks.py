0ae7c5db708a39e8a2b0944f87c9b0a436df0ca6
noam2003 <noammiz918@gmail.com>
2021-01-21 13:59:29 +0200

    remove useless documentation and add to the definition of function -> RiskLevels

--- detective/toolbox/lenses/xss/advanced_checks.py
+++ detective/toolbox/lenses/xss/advanced_checks.py
@@ -1,19 +1,12 @@
 import re
+import toml
 from urllib.parse import urlparse
 from detective.toolbox.risk_levels import RiskLevels
 
 
 class AdvancedChecks:
     @staticmethod
-    def blind_xss(request):
-        """
-        This function will check if the user tried to
-        inject a website to the server
-        :param request: the user's request
-        :type request: string
-        :return: the dangerous level according to the findings
-        :rtype: enum RiskLevels
-        """
+    def blind_xss(request) -> RiskLevels:
         urls_found = re.findall(r"""(?:<|')\s*(?:i?frame|img|embed|ipt)(?:/|\s).*?src\s*=\s*(?:(?:\"|'|`)\s*)?(?:<\s*)?(?P<url>[^\"'`<>]+)""", request)
         urls_found.append(re.findall(r"""<\s*link(?:/|\s).*?href\s*=\s*(?:(?:\"|'|`)\s*)?(?:<\s*)?(?P<url>[^\"'`<>]+)""", request))
         urls_found.append(re.findall(r"""<\s*meta(?:/|\s).*?content\s*=\s*(?:(?:\"|'|`)\s*)?(?:<\s*)?(?P<url>[^\"'`<>]+)""", request))
@@ -23,9 +16,10 @@ class AdvancedChecks:
         urls_found.append(re.findall(r"""<\s*a(?:/|\s).*?href\s*=\s*(?:(?:\"|'|`)\s*)?(?:javascript\s*:\s*document\.location\s*=\s*(?:(?:\"|'|`)\s*)?)?(?:<\s*)?(?P<url>[^\"'`<>]+)""", request))
         if urls_found is not None:
             white_spaces = re.compile(r"\s+")
+            server_url = toml.load("server_info.toml")["host"]
             for url in urls_found:
                 parse_result = urlparse(re.sub(white_spaces, '', url))
-                if parse_result.netloc != '':
+                if parse_result.netloc != '' and url not in server_url:
                     return RiskLevels.CATASTROPHIC
             return RiskLevels.MODERATE
         return RiskLevels.NO_RISK
