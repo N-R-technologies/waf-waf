0c4837176a60957c495cc14a4c02ed93064563d5
Ron Konis <ronkonis1@gmail.com>
2021-01-21 16:29:57 +0200

    updated the advanced check in the LFI lens

--- detective/toolbox/lenses/local_file_inclusion/advanced_checks.py
+++ detective/toolbox/lenses/local_file_inclusion/advanced_checks.py
@@ -8,12 +8,12 @@ class AdvancedChecks:
         malicious_parameters = ["cat", "dir", "action", "board", "date", "detail", "file", "download",
                                 "path", "folder", "prefix", "include", "page", "inc", "locate", "show",
                                 "doc", "site", "type", "view", "content", "document", "layout", "mod", "conf"]
-        detect_parameters = re.findall(r"""\?(?P<parameter>.+)=\s*.+?""", request)
-        if detect_parameters is not None:
+        detect_parameters_result = re.findall(r"""\?(?P<parameter>.+)=\s*.+?""", request)
+        if detect_parameters_result is not None:
             white_spaces = re.compile(r"\s+")
-            for parameter in detect_parameters:
+            for parameter in detect_parameters_result:
                 parameter = re.sub(white_spaces, '', parameter)
-                for extension in malicious_parameters:
-                    if extension in parameter:
-                        return RiskLevels.MODERATE
+                for malicious_parameter in malicious_parameters:
+                    if malicious_parameter in parameter:
+                        return RiskLevels.SLIGHT
         return RiskLevels.NO_RISK
