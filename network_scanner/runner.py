2a28a1daf80dfc84684b08be82a513775e157c24
Ron Konis <ronkonis1@gmail.com>
2021-01-27 21:10:05 +0000

    updated execute_operation documentation

--- network_scanner/runner.py
+++ network_scanner/runner.py
@@ -6,15 +6,18 @@ class Runner:
 
     def execute_operation(self, loading_str, color, operation_to_run, *args):
         """
-        :param loading_str: the string represent when loading
-        :param color: the color of the loading
-        :param operation_to_run: pointer to the function to run
-        :param args: the arguments for the function
-        :type loading_str: str
-        :type color: class color
-        :type operation_to_run: class function
+        This function will start and stop the loading animation,
+        and will execute the given function with its given parameters
+        :param loading_str: the string to display on the loading animation
+        :param color: the color of the loaded string
+        :param operation_to_run: a pointer to the executed function
+        :param *args: the packed parameters of the executed function
+        :type loading_str: string
+        :type color: Color
+        :type operation_to_run: function
         :type args: tuple
-        :return: the return type of the function to operate
+        :return: the result of the executed function
+        :rtype: string, boolean or None (depends on which function is being executed)
         """
         self._loader.start_loading(loading_str, color)
         operation_output = operation_to_run(*args)
