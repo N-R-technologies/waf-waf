31f36baeed3fda3bc3c36f9f163220ff432cadc6
Ron Konis <ronkonis1@gmail.com>
2021-01-29 01:19:05 +0200

    added tkinter input box for the remove email function

--- email_manager.py
+++ email_manager.py
@@ -32,9 +32,11 @@ class EmailManager:
         This function will add an email to the configuration file
         :param name: the email owner name
         :param address: the email address
-        :type name: string
-        :type address: string
+        :type name: tkinter.Entry
+        :type address: tkinter.Entry
         """
+        name = name.get()
+        address = address.get()
         valid_email = True
         if not os.path.exists(self.USER_EMAILS_FILE_PATH):
             with open(self.USER_EMAILS_FILE_PATH, 'w') as email_file:
@@ -42,13 +44,13 @@ class EmailManager:
                 email_file.close()
         user_emails = toml.load(self.USER_EMAILS_FILE_PATH).get("emails", {})
         if name == "":
-            messagebox.showerror("Invalid Name", f"Please enter a name for the user")
+            messagebox.showerror("Invalid Name", "Please enter a name for the user")
             valid_email = False
         elif not self._is_valid_name(name, user_emails.keys()):
             messagebox.showerror("Invalid Name", f"A user named {name} already exists!")
             valid_email = False
         if not self._is_valid_address(address):
-            messagebox.showerror("Invalid Email", "Your address is not valid!")
+            messagebox.showerror("Invalid Email", "Address is not valid!")
             valid_email = False
         if valid_email:
             with open(self.USER_EMAILS_FILE_PATH, 'a') as email_file:
@@ -62,36 +64,47 @@ class EmailManager:
         This function will call the add_email function
         with the appropriate parameters
         """
-        print("enter the input in the message box")
-        self._email_manager_menu.get_input(self.add_email, "Add New Email", "Name", "Address")
+        self._email_manager_menu.get_input(self.add_email, "Add New Email", "", "Name", "Address")
 
-    def remove_email(self):
+    def remove_email(self, email_index):
         """
         This function will remove an email from the configuration file
+        :param email_index: the index of the email to remove in the list
+        :type email_index: tkinter.Entry
+        """
+        email_index = email_index.get()
+        user_emails = toml.load(self.USER_EMAILS_FILE_PATH).get("emails", {})
+        if not email_index.isdigit():
+            messagebox.showerror("Invalid Input", "Please enter a number")
+        else:
+            email_index = int(email_index)
+            if not 1 <= email_index <= len(user_emails):
+                messagebox.showerror("Invalid Input", "Index out of range!")
+            else:
+                index = 1
+                user_name = ""
+                for name in user_emails.keys():
+                    if index == email_index:
+                        del user_emails[name]
+                        user_name = name
+                        break
+                    index += 1
+                with open(self.USER_EMAILS_FILE_PATH, 'w') as email_file:
+                    toml.dump(toml.loads("[emails]"), email_file)
+                    for name, address in user_emails.items():
+                        toml.dump({name: address}, email_file)
+                    email_file.close()
+                messagebox.showinfo("Success", f"Successfully removed {user_name} from the list!")
+        self._email_manager_menu.close_input()
+
+    def call_remove_email(self):
+        """
+        This function will call the add_email function
+        with the appropriate parameters
         """
         self.display_emails()
-        exit_flag = False
-        user_emails = toml.load(self.USER_EMAILS_FILE_PATH).get("emails", {})
-        email_index = input("\nEnter the index of the email which you want to remove:\n")
-        while not (email_index.isdigit() and 1 <= int(email_index) <= len(user_emails)) or exit_flag:
-            print("Invalid index")
-            print("Press Q to quit")
-            email_index = input("Enter the index of the email which you want to remove:\n")
-            if email_index.lower() == 'q':
-                exit_flag = True
-        if exit_flag:
-            return
-        index = 1
-        for name in user_emails.keys():
-            if index == int(email_index):
-                del user_emails[name]
-                break
-            index += 1
-        with open(self.USER_EMAILS_FILE_PATH, 'w') as email_file:
-            toml.dump(toml.loads("[emails]"), email_file)
-            for name, address in user_emails.items():
-                toml.dump({name: address}, email_file)
-            email_file.close()
+        print("\nPlease enter the index of the email you wish to remove")
+        self._email_manager_menu.get_input(self.remove_email, "Remove Email", "", "Index")
 
     def _is_valid_address(self, address):
         """
@@ -125,7 +138,7 @@ class EmailManager:
         """
         self._email_manager_menu.add_option("1. Display all the emails", self.display_emails)
         self._email_manager_menu.add_option("2. Add an email to the list", self.call_add_email)
-        self._email_manager_menu.add_option("3. Delete an email from the list", self.remove_email)
+        self._email_manager_menu.add_option("3. Delete an email from the list", self.call_remove_email)
         self._email_manager_menu.add_option("4. Exit (or simply press Q)", "exit")
         for menu_item in range(len(self._email_manager_menu.menu)):
             if self._email_manager_menu.controller[menu_item] == 1:
