9a4db7e2b998d1bb99b420ffcabbb557e87ed76f
Ron Konis <ronkonis1@gmail.com>
2021-02-03 20:34:23 +0200

    added the base code for the command injection check

---
+++ detective/toolbox/lenses/command_injection/advanced_checks.py
@@ -0,0 +1,20 @@
+import re
+from detective.toolbox.risk_levels import RiskLevels
+
+REGEX_QUERY = 0
+RISK_LEVEL = 1
+
+
+class AdvancedChecks:
+    @staticmethod
+    def malicious_commands(request) -> RiskLevels:
+        malicious_commands = (("(?:whoami|ls)", RiskLevels.MODERATE), ("(?:uname\s+-a|ver)", RiskLevels.CRITICAL),
+                              ("i[fp]config(?:\s+\/all)?", RiskLevels.CATASTROPHIC), ("netstat\s+-an", RiskLevels.CRITICAL),
+                              ("(?:(?:rm|ps)\s+-.+|tasklist)", RiskLevels.CATASTROPHIC), ("(?:ping|echo)\s+.+", RiskLevels.SLIGHT),
+                              ("nslookup\s+.+", RiskLevels.CATASTROPHIC), ("cat\s+/etc/.+", RiskLevels.CATASTROPHIC))
+        detect_commands_result = re.findall(r"""(?:&{1,2}|\|{1,2}|;|\n|0x0a)\s*(?:`|\$\s*\()?(?P<command>.+)""", request)
+        for command in detect_commands_result:
+            for malicious_command in malicious_commands:
+                if re.search(malicious_command[REGEX_QUERY], command):
+                    return malicious_command[RISK_LEVEL]
+        return RiskLevels.NO_RISK
---
+++ detective/toolbox/lenses/command_injection/info.py
@@ -0,0 +1,12 @@
+category = "Command Injection"
+
+general_info = "Command Injection is "
+
+deep_info = {
+    "malicious_commands": " * Malicious Commands: An attacker have tried to inject commands into your system.\n  "
+                          "Some commands might be dangerous than other, but you should not allow\n  "
+                          "users to enter any kind of commands into your system.\n"
+}
+
+links_for_info = "For more information about Command Injection, check the following links:\n" \
+                 "\n"
