41feb345095e61c88776f5a371605111e5ec9c55
noam2003 <noammiz918@gmail.com>
2021-02-10 13:34:30 +0200

    replace the one advanced check with a lot of basic checks

--- detective/toolbox/lenses/command_injection/advanced_checks.py
+++ detective/toolbox/lenses/command_injection/advanced_checks.py
@@ -1,25 +1,4 @@
-import re
-from detective.toolbox.risk_levels import RiskLevels
-
-REGEX_QUERY = 0
-RISK_LEVEL = 1
 
 
 class AdvancedChecks:
-    @staticmethod
-    def malicious_commands(request) -> RiskLevels:
-        malicious_commands = (("(?:whoami|ls)", RiskLevels.MODERATE), ("(?:uname\s+-a)", RiskLevels.CRITICAL),
-                              ("i[fp]config(?:\s+\/all)?", RiskLevels.CATASTROPHIC), ("netstat\s+-an", RiskLevels.CRITICAL),
-                              ("(?:(?:rm|ps)\s+-.+|tasklist)", RiskLevels.CATASTROPHIC), ("(?:ping|echo)\s+.+", RiskLevels.SLIGHT),
-                              ("nslookup\s+.+", RiskLevels.CATASTROPHIC), ("cat.*/etc/.+", RiskLevels.CATASTROPHIC),
-                              ("nc\s+-", RiskLevels.MODERATE), ("pwd($|\s+)", RiskLevels.MODERATE), ("cat.*/", RiskLevels.SLIGHT),
-                              ("id\s*(-g|-G|-n|-r|-u)", RiskLevels.CRITICAL), ("touch|rm|cp", RiskLevels.MODERATE), ("sleep\s+\d", RiskLevels.SLIGHT),
-                              ("php\s*-[abcndefhilmrstvwz]", RiskLevels.CRITICAL), ("php\s*--(ini|rf|rc|re|rz|ri)", RiskLevels.CRITICAL),
-                              ("php\s+-version", RiskLevels.CRITICAL), ("curl|wget", RiskLevels.CRITICAL))
-        detect_commands_result = re.findall(r"""(?:&{1,2}|\|{1,2}|;|\n|0x0a)\s*(?:`|\$\s*\()?(?P<command>(?:(?!&|\||\n|0x0a|;).)+)""", request)
-        max_risk_level = 0
-        for command in detect_commands_result:
-            for malicious_command in malicious_commands:
-                if re.search(malicious_command[REGEX_QUERY], command) and malicious_command[RISK_LEVEL] > max_risk_level:
-                    max_risk_level = malicious_command[RISK_LEVEL]
-        return max_risk_level
+    pass
--- detective/toolbox/lenses/command_injection/basic_checks.py
+++ detective/toolbox/lenses/command_injection/basic_checks.py
@@ -1,2 +1,104 @@
+import re
+from detective.toolbox.risk_levels import RiskLevels
+
+
 class BasicChecks:
-    pass
+
+    @staticmethod
+    def preparation(request) -> list:
+        return re.findall(r"""(?:&{1,2}|\|{1,2}|;|\n|0x0a)\s*(?:`|\$\s*\()?(?P<command>(?:(?!&|\||\n|0x0a|;|`|\)).)+)""", request)
+
+    @staticmethod
+    def server_information(command) -> RiskLevels:
+        return RiskLevels.MODERATE if re.search("(?:whoami|ls)", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def server_sensitive_information(command) -> RiskLevels:
+        return RiskLevels.CRITICAL if re.search("(?:uname\s+-a)", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def network_information(command) -> RiskLevels:
+        return RiskLevels.CATASTROPHIC if re.search("i[fp]config(?:\s+\/all)?", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def network_statistics(command) -> RiskLevels:
+        return RiskLevels.CRITICAL if re.search("netstat\s+-an", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def process_information(command) -> RiskLevels:
+        return RiskLevels.CATASTROPHIC if re.search("ps\s+-.+|tasklist", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def basic_terminal_commands(command) -> RiskLevels:
+        return RiskLevels.SLIGHT if re.search("(?:ping|echo)\s+.+", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def nslookup(command) -> RiskLevels:
+        return RiskLevels.CATASTROPHIC if re.search("nslookup\s+.+", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def show_sensitive_file(command) -> RiskLevels:
+        return RiskLevels.CATASTROPHIC if re.search("cat.*/etc/.+", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def netcat_communication(command) -> RiskLevels:
+        return RiskLevels.MODERATE if re.search("nc\s+-", command) \
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def server_running_path(command) -> RiskLevels:
+        return RiskLevels.MODERATE if re.search("pwd($|\s+)", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def show_file(command) -> RiskLevels:
+        return RiskLevels.SLIGHT if re.search("cat.*/", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def server_groups(command) -> RiskLevels:
+        return RiskLevels.CRITICAL if re.search("id\s*(-g|-G|-n|-r|-u|$)", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def modify_file(command) -> RiskLevels:
+        return RiskLevels.MODERATE if re.search("touch|cp|rm", command) \
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def delete_file(command) -> RiskLevels:
+        return RiskLevels.CRITICAL if re.search("rm\s*-", command) \
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def sleep_server(command) -> RiskLevels:
+        return RiskLevels.SLIGHT if re.search("sleep\s+\d", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def php_single_char_flag(command) -> RiskLevels:
+        return RiskLevels.CRITICAL if re.search("php\s*-[abcndefhilmrstvwz]", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def php_multiply_char_flag(command) -> RiskLevels:
+        return RiskLevels.CRITICAL if re.search("php\s*--(ini|rf|rc|re|rz|ri)", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def php_info(command) -> RiskLevels:
+        return RiskLevels.MODERATE if re.search("php\s+-version", command)\
+            else RiskLevels.NO_RISK
+
+    @staticmethod
+    def upload_download_files_from_internet(command) -> RiskLevels:
+        return RiskLevels.CATASTROPHIC if re.search("curl|wget", command)\
+            else RiskLevels.NO_RISK
