dddc8293c3e4b5e8ea2a3b09350f915b12163d72
noam2003 <noammiz918@gmail.com>
2021-02-10 14:14:36 +0200

    iterate every command in the commands list in every basic check

--- detective/toolbox/lenses/command_injection/basic_checks.py
+++ detective/toolbox/lenses/command_injection/basic_checks.py
@@ -9,96 +9,134 @@ class BasicChecks:
         return re.findall(r"""(?:&{1,2}|\|{1,2}|;|\n|0x0a)\s*(?:`|\$\s*\()?(?P<command>(?:(?!&|\||\n|0x0a|;|`|\)).)+)""", request)
 
     @staticmethod
-    def server_information(command) -> RiskLevels:
-        return RiskLevels.MODERATE if re.search("(?:whoami|ls)", command)\
-            else RiskLevels.NO_RISK
+    def server_information(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("(?:whoami|ls)", command):
+                return RiskLevels.MODERATE
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def server_sensitive_information(command) -> RiskLevels:
-        return RiskLevels.CRITICAL if re.search("(?:uname\s+-a)", command)\
-            else RiskLevels.NO_RISK
+    def server_sensitive_information(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("(?:uname\s+-a)", command):
+                return RiskLevels.CRITICAL
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def network_information(command) -> RiskLevels:
-        return RiskLevels.CATASTROPHIC if re.search("i[fp]config(?:\s+\/all)?", command)\
-            else RiskLevels.NO_RISK
+    def network_information(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("i[fp]config(?:\s+\/all)?", command):
+                return RiskLevels.CATASTROPHIC
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def network_statistics(command) -> RiskLevels:
-        return RiskLevels.CRITICAL if re.search("netstat\s+-an", command)\
-            else RiskLevels.NO_RISK
+    def network_statistics(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("netstat\s+-an", command):
+                return RiskLevels.CRITICAL
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def process_information(command) -> RiskLevels:
-        return RiskLevels.CATASTROPHIC if re.search("ps\s+-.+|tasklist", command)\
-            else RiskLevels.NO_RISK
+    def process_information(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("ps\s+-.+|tasklist", command):
+                return RiskLevels.CATASTROPHIC
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def basic_terminal_commands(command) -> RiskLevels:
-        return RiskLevels.SLIGHT if re.search("(?:ping|echo)\s+.+", command)\
-            else RiskLevels.NO_RISK
+    def basic_terminal_commands(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("(?:ping|echo)\s+.+", command):
+                return RiskLevels.SLIGHT
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def nslookup(command) -> RiskLevels:
-        return RiskLevels.CATASTROPHIC if re.search("nslookup\s+.+", command)\
-            else RiskLevels.NO_RISK
+    def nslookup(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("nslookup\s+.+", command):
+                return RiskLevels.CATASTROPHIC
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def show_sensitive_file(command) -> RiskLevels:
-        return RiskLevels.CATASTROPHIC if re.search("cat.*/etc/.+", command)\
-            else RiskLevels.NO_RISK
+    def show_sensitive_file(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("cat.*/etc/.+", command):
+                return RiskLevels.CATASTROPHIC
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def netcat_communication(command) -> RiskLevels:
-        return RiskLevels.MODERATE if re.search("nc\s+-", command) \
-            else RiskLevels.NO_RISK
+    def netcat_communication(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("nc\s+-", command):
+                return RiskLevels.MODERATE
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def server_running_path(command) -> RiskLevels:
-        return RiskLevels.MODERATE if re.search("pwd($|\s+)", command)\
-            else RiskLevels.NO_RISK
+    def server_running_path(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("pwd($|\s+)", command):
+                return RiskLevels.MODERATE
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def show_file(command) -> RiskLevels:
-        return RiskLevels.SLIGHT if re.search("cat.*/", command)\
-            else RiskLevels.NO_RISK
+    def show_file(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("cat.*/", command):
+                return RiskLevels.SLIGHT
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def server_groups(command) -> RiskLevels:
-        return RiskLevels.CRITICAL if re.search("id\s*(-g|-G|-n|-r|-u|$)", command)\
-            else RiskLevels.NO_RISK
+    def server_groups(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("id\s*(-g|-G|-n|-r|-u|$)", command):
+                return RiskLevels.CRITICAL
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def modify_file(command) -> RiskLevels:
-        return RiskLevels.MODERATE if re.search("touch|cp|rm", command) \
-            else RiskLevels.NO_RISK
+    def modify_file(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("touch|cp|rm", command):
+                return RiskLevels.MODERATE
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def delete_file(command) -> RiskLevels:
-        return RiskLevels.CRITICAL if re.search("rm\s*-", command) \
-            else RiskLevels.NO_RISK
+    def delete_file(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("rm\s*-", command):
+                return RiskLevels.CRITICAL
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def sleep_server(command) -> RiskLevels:
-        return RiskLevels.SLIGHT if re.search("sleep\s+\d", command)\
-            else RiskLevels.NO_RISK
+    def sleep_server(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+                if re.search("sleep\s+\d", command):
+                    return RiskLevels.SLIGHT
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def php_single_char_flag(command) -> RiskLevels:
-        return RiskLevels.CRITICAL if re.search("php\s*-[abcndefhilmrstvwz]", command)\
-            else RiskLevels.NO_RISK
+    def php_single_char_flag(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("php\s*-[abcndefhilmrstvwz]", command):
+                return RiskLevels.CRITICAL
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def php_multiply_char_flag(command) -> RiskLevels:
-        return RiskLevels.CRITICAL if re.search("php\s*--(ini|rf|rc|re|rz|ri)", command)\
-            else RiskLevels.NO_RISK
+    def php_multiply_char_flag(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("php\s*--(ini|rf|rc|re|rz|ri)", command):
+                return RiskLevels.CRITICAL
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def php_info(command) -> RiskLevels:
-        return RiskLevels.MODERATE if re.search("php\s+-version", command)\
-            else RiskLevels.NO_RISK
+    def php_info(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("php\s+-version", command):
+                return RiskLevels.MODERATE
+        return RiskLevels.NO_RISK
 
     @staticmethod
-    def upload_download_files_from_internet(command) -> RiskLevels:
-        return RiskLevels.CATASTROPHIC if re.search("curl|wget", command)\
-            else RiskLevels.NO_RISK
+    def upload_download_files_from_internet(potential_commands_list) -> RiskLevels:
+        for command in potential_commands_list:
+            if re.search("curl|wget", command):
+                return RiskLevels.CATASTROPHIC
+        return RiskLevels.NO_RISK
