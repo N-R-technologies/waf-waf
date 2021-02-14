55fc9e3007a1658a3dc0a90ee61a3e0d2d68785a
Ron Konis <ronkonis1@gmail.com>
2021-02-14 20:11:26 +0200

    added log file path constant variable

--- attacks_logger/logger.py
+++ attacks_logger/logger.py
@@ -1,25 +1,23 @@
 import os
-from datetime import date
 import toml
+from datetime import date
 
 
 class AttacksLogger:
+    ATTACKS_LOG_FILE_PATH = "attacks.toml"
 
     def __init__(self):
-        open("attacks.toml", 'r').close()
+        open(self.ATTACKS_LOG_FILE_PATH, 'r').close()
 
-    def add_attack_attempt_to_log(self, attack_content, attacker_ip):
-        file_name = "attacks.toml"
-        if not os.path.exists(file_name):
-            open(file_name, 'w').close()
-        attacks = list(toml.load(file_name).get(date.today().strftime("%d/%m/%Y"), []))
+    def add_attack_attempt(self, attacker_ip, attack_content):
+        if not os.path.exists(self.ATTACKS_LOG_FILE_PATH):
+            open(self.ATTACKS_LOG_FILE_PATH, 'w').close()
+        attacks = list(toml.load(self.ATTACKS_LOG_FILE_PATH).get(date.today().strftime("%d/%m/%Y"), []))
         attacks.append((attacker_ip, attack_content))
-        toml.dump({date.today().strftime("%d/%m/%Y"): [(attacker_ip, attack_content)]}, file_name)
+        toml.dump({date.today().strftime("%d/%m/%Y"): [(attacker_ip, attack_content)]}, self.ATTACKS_LOG_FILE_PATH)
 
-    def clear_log_file(self):
-        open("attacks.toml", 'w').close()
+    def get_attack(self, attacker_ip, attack_date):
+        return list(toml.load(self.ATTACKS_LOG_FILE_PATH).get(date.today().strftime("%d/%m/%Y"), []))
 
-    def get_specific_attack(self, date, ip) -> list:
-        return list(toml.load("attacks.toml").get(date.today().strftime("%d/%m/%Y"), []))
-
-
+    def clear_log(self):
+        open(self.ATTACKS_LOG_FILE_PATH, 'w').close()
