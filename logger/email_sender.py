bdc5da034d0a74448b6910cc65c23f819127c8b8
Ron Konis <ronkonis1@gmail.com>
2021-03-29 16:45:22 +0300

    added icons to the notifier

--- logger/email_sender.py
+++ logger/email_sender.py
@@ -10,9 +10,10 @@ import notify2
 
 
 class EmailSender:
-    LOG_FILE_PATH = "logger/data/logs/daily_log_"
     BOT_EMAIL_FILE_PATH = "logger/data/bot_email.toml"
     USER_EMAILS_FILE_PATH = "logger/data/user_emails.toml"
+    ICONS_FILE_PATH = os.getcwd() + "/misc/icons/"
+    LOG_FILE_PATH = "logger/data/logs/daily_log_"
     LOG_DESCRIPTION_FILE_PATH = "logger/data/log_description.txt"
     LOG_SUBJECT = "Daily Log - "
 
@@ -25,39 +26,40 @@ class EmailSender:
     def send_log(self):
         user_addresses = self._load_user_addresses_configuration(self.USER_EMAILS_FILE_PATH)
         if len(user_addresses) > 0:
-            daily_log_mail = MIMEMultipart()
-            daily_log_mail["From"] = self._bot_address
-            daily_log_mail["To"] = COMMASPACE.join(user_addresses)
-            daily_log_mail["Date"] = formatdate(localtime=True)
-            daily_log_mail["Subject"] = self.LOG_SUBJECT + date.today().strftime("%d/%m/%Y")
+            daily_log = MIMEMultipart()
+            daily_log["From"] = self._bot_address
+            daily_log["To"] = COMMASPACE.join(user_addresses)
+            daily_log["Date"] = formatdate(localtime=True)
+            daily_log["Subject"] = self.LOG_SUBJECT + date.today().strftime("%d/%m/%Y")
             with open(self.LOG_DESCRIPTION_FILE_PATH, 'r') as log_description_file:
                 daily_log_description = log_description_file.read()
                 log_description_file.close()
-            daily_log_mail.attach(MIMEText(daily_log_description))
+            daily_log.attach(MIMEText(daily_log_description))
 
             daily_log_path = self.LOG_FILE_PATH + date.today().strftime("%d_%m_%Y") + ".pdf"
-            with open(daily_log_path, "rb") as daily_log:
-                part = MIMEApplication(daily_log.read(), Name=os.path.basename(daily_log_path))
-            part["Content-Disposition"] = 'attachment; filename="%s"' % os.path.basename(daily_log_path)
-            daily_log_mail.attach(part)
+            with open(daily_log_path, "rb") as daily_log_file:
+                log_file = MIMEApplication(daily_log_file.read(), Name=os.path.basename(daily_log_path))
+            log_file["Content-Disposition"] = 'attachment; filename="%s"' % os.path.basename(daily_log_path)
+            daily_log.attach(log_file)
 
             try:
                 server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
                 server_ssl.ehlo()
                 server_ssl.login(self._bot_address, self._bot_pass)
-                does_not_get_email = server_ssl.sendmail(self._bot_address, list(user_addresses), daily_log_mail.as_string())
+                refused_recipients = server_ssl.sendmail(self._bot_address, list(user_addresses), daily_log.as_string())
                 server_ssl.close()
-                if len(does_not_get_email) == 0:
-                    self._notify_user("Email Sent!", "Your daily log was sent successfully", "success.png")
+                if len(refused_recipients) == 0:
+                    self._notify_user("Successful Email!", "Successfully sent the daily log", "mail.png")
                 else:
-                    self._notify_user("Email Error!", f"Your daily log wasn't sent successfully to the following users: "
-                                                      f"{str(does_not_get_email)}", "warning_sign.png")
-            except smtplib.SMTPRecipientsRefused:
-                self._notify_user("Email Error", "Couldn't deliver emails to the recipients because they refused", "warning_sign.png")
+                    self._notify_user("Refuse Error!", "The following recipients have refused to receive the daily log:"
+                                                       f"\n{str(refused_recipients.keys())}", "refuse.png")
             except smtplib.SMTPAuthenticationError:
-                self._notify_user("Email Error", "Couldn't authenticate to the waf waf gmail account", "warning_sign.png")
+                self._notify_user("Authentication Error!", "WAF WAF account failed to authenticate\n"
+                                                           "Please contact the manufacturers", "lock.png")
+            except smtplib.SMTPRecipientsRefused:
+                self._notify_user("Refuse Error!", "All recipients have refused to receive the daily log", "refuse.png")
             except smtplib.SMTPException:
-                self._notify_user("Email Error", "Couldn't send emails properly", "warning_sign.png")
+                self._notify_user("Unexpected Error!", "An error has occurred while sending the daily log", "error.png")
 
     def _load_bot_email_configuration(self, bot_email_file_path):
         if os.path.exists(bot_email_file_path):
@@ -77,9 +79,9 @@ class EmailSender:
             user_addresses.add(address)
         return user_addresses
 
-    def _notify_user(self, title, content, image_name):
+    def _notify_user(self, title, content, icon_name):
         notify2.init("WAF WAF")
-        picture_path = os.getcwd() + "/misc/" + image_name
-        notifier = notify2.Notification(title, content, picture_path)
+        icon_path = self.ICONS_FILE_PATH + icon_name
+        notifier = notify2.Notification(title, content, icon_path)
         notifier.set_timeout(5000)
         notifier.show()
